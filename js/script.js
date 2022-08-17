cfc33153df734d9332374e5551c0aa5e501c669e
Roman <rk-info@mail.ru>
2022-08-17 18:43:19 +0300

    new commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const storeName = "magazine";
+let storeDescription = {
+    budget: 10000,
+    employees: ["Ivan", "Roman", "Sergey"],
+    products: {
+        apple: 100,
+        orang: 200
+    },
+    open: true
+};
+
+
+const category = "toys";
+console.log(`https://somur.com/${category}/5`);
+
+
+const user = "ivan";
+alert(`привет, ${user}`);
+
+console.log(4 + '5');
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(--incr);
+console.log(++decr);
+
+console.log(2 * 4 == 4)
+const isCheced = true,
+    isCloud = true;
